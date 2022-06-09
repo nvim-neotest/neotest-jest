af237a93d0a4a5b9ca0799667beb9acfe19df7f0
Hayden Meade <haydenmeade@gmail.com>
2022-06-09 13:42:06 +1000

    cleanup and fixes

--- lua/neotest-jest/init.lua
+++ lua/neotest-jest/init.lua
@@ -1,22 +1,22 @@
 ---@diagnostic disable: undefined-field
-local lib = require "neotest.lib"
-local logger = require "neotest.logging"
+local lib = require('neotest.lib')
+local logger = require('neotest.logging')
 
 ---@type neotest.Adapter
-local adapter = { name = "neotest-jest" }
+local adapter = { name = 'neotest-jest' }
 
-adapter.root = lib.files.match_root_pattern "package.json"
+adapter.root = lib.files.match_root_pattern('package.json')
 
 function adapter.is_test_file(file_path)
   if file_path == nil then
     return false
   end
-  if string.match(file_path, "__tests__") then
+  if string.match(file_path, '__tests__') then
     return true
   end
-  for _, x in ipairs { "spec", "test" } do
-    for _, ext in ipairs { "js", "jsx", "coffee", "ts", "tsx" } do
-      if string.match(file_path, x .. "%." .. ext .. "$") then
+  for _, x in ipairs({ 'spec', 'test' }) do
+    for _, ext in ipairs({ 'js', 'jsx', 'coffee', 'ts', 'tsx' }) do
+      if string.match(file_path, x .. '%.' .. ext .. '$') then
         return true
       end
     end
@@ -29,13 +29,17 @@ end
 function adapter.discover_positions(path)
   local query = [[
   ((call_expression
-      function: (identifier) @func_name (#match? @func_name "^describe$")
+      function: (identifier) @func_name (#match? @func_name "^describe")
       arguments: (arguments (_) @namespace.name (arrow_function))
   )) @namespace.definition
 
 
   ((call_expression
-      function: (identifier) @func_name (#match? @func_name "^it$")
+      function: (identifier) @func_name (#match? @func_name "^it")
+      arguments: (arguments (_) @test.name (arrow_function))
+  ) ) @test.definition
+  ((call_expression
+      function: (member_expression) @func_name (#match? @func_name "^it")
       arguments: (arguments (_) @test.name (arrow_function))
   ) ) @test.definition
     ]]
@@ -45,9 +49,7 @@ end
 ---@param args neotest.RunArgs
 ---@return neotest.RunSpec | nil
 function adapter.build_spec(args)
-  --logger.debug("buildspec", args, "args")
-  --lib.
-  local results_path = vim.fn.tempname() .. ".json"
+  local results_path = vim.fn.tempname() .. '.json'
   local tree = args.tree
   if not tree then
     return
@@ -58,27 +60,27 @@ function adapter.build_spec(args)
   --roots
   -- A list of paths to directories that Jest should use to search for files in.
   -- end
-  local testNamePattern = ".*"
-  if pos.type == "test" then
+  local testNamePattern = '.*'
+  if pos.type == 'test' then
     testNamePattern = pos.name
   end
 
-  local binary = "jest"
-  if vim.fn.filereadable "node_modules/.bin/jest" then
-    binary = "node_modules/.bin/jest"
+  local binary = 'jest'
+  if vim.fn.filereadable('node_modules/.bin/jest') then
+    binary = 'node_modules/.bin/jest'
   end
 
-  local command = vim.tbl_flatten {
+  local command = vim.tbl_flatten({
     binary,
-    "--no-coverage",
-    "--testLocationInResults",
-    "--verbose",
-    "--json",
-    "--outputFile=" .. results_path,
-    "--testNamePattern=" .. testNamePattern,
-    "--runTestsByPath",
+    '--no-coverage',
+    '--testLocationInResults',
+    '--verbose',
+    '--json',
+    '--outputFile=' .. results_path,
+    '--testNamePattern=' .. testNamePattern,
+    '--runTestsByPath',
     pos.path,
-  }
+  })
   return {
     command = command,
     context = {
@@ -90,19 +92,61 @@ end
 
 local function cleanAnsi(s)
   return s
-      :gsub("\x1b%[%d+;%d+;%d+;%d+;%d+m", "")
-      :gsub("\x1b%[%d+;%d+;%d+;%d+m", "")
-      :gsub("\x1b%[%d+;%d+;%d+m", "")
-      :gsub("\x1b%[%d+;%d+m", "")
-      :gsub("\x1b%[%d+m", "")
+      :gsub('\x1b%[%d+;%d+;%d+;%d+;%d+m', '')
+      :gsub('\x1b%[%d+;%d+;%d+;%d+m', '')
+      :gsub('\x1b%[%d+;%d+;%d+m', '')
+      :gsub('\x1b%[%d+;%d+m', '')
+      :gsub('\x1b%[%d+m', '')
 end
 
 local function findErrorLine(line, errStr)
-  local _, _, errLine = string.find(errStr, "(%d+)%:%d+")
+  local _, _, errLine = string.find(errStr, '(%d+)%:%d+')
   if errLine then
     return errLine - 1
   end
   return line
+end
+
+local function parsed_json_to_results(data, output_file)
+  local tests = {}
+  local failed = false
+
+  local testFn = data.testResults[1].name
+  for _, result in pairs(data.testResults[1].assertionResults) do
+    local status, name = result.status, result.title
+    if name == nil then
+      logger.error('Failed to find parsed test result ', result)
+      return {}, failed
+    end
+    local keyid = testFn
+    for _, value in ipairs(result.ancestorTitles) do
+      keyid = keyid .. '::' .. '"' .. value .. '"'
+    end
+    keyid = keyid .. '::' .. '"' .. name .. '"'
+    if status == 'pending' then
+      status = 'skipped'
+    end
+    tests[keyid] = {
+      status = status,
+      short = name .. ': ' .. status,
+      output = output_file,
+      location = result.location,
+    }
+    if result.failureMessages then
+      failed = true
+      local errors = {}
+      for i, failMessage in ipairs(result.failureMessages) do
+        local msg = cleanAnsi(failMessage)
+        errors[i] = {
+          line = findErrorLine(result.location.line - 1, msg),
+          message = msg,
+        }
+        tests[keyid].short = tests[keyid].short .. '\n' .. msg
+      end
+      tests[keyid].errors = errors
+    end
+  end
+  return tests, failed
 end
 
 ---@async
@@ -111,73 +155,32 @@ end
 ---@param tree neotest.Tree
 ---@return neotest.Result[]
 function adapter.results(spec, _, tree)
-  vim.pretty_print(spec)
   local output_file = spec.context.results_path
   local success, data = pcall(lib.files.read, output_file)
   if not success then
-    vim.schedule(function() -- FIXME: Report global errors correctly
-      vim.notify "no file"
-    end)
+    logger.error('No test output file found ', output_file)
     return {}
   end
   local ok, parsed = pcall(vim.json.decode, data, { luanil = { object = true } })
   if not ok then
-    vim.schedule(function() -- FIXME: Report global errors correctly
-      vim.notify("Failed to parse json: " .. parsed)
-    end)
+    logger.error('Failed to parse test output json ', output_file)
     return {}
   end
 
-  local tests = {}
-
-  local testFn = parsed.testResults[1].name
-  for _, result in pairs(parsed.testResults[1].assertionResults) do
-    vim.pretty_print(result)
-    local status, name = result.status, result.title
-    if name == nil then
-      vim.schedule(function() -- FIXME: Report global errors correctly
-        vim.notify("Failed to get test result: " .. parsed)
-      end)
-      return {}
-    end
-    local keyid = testFn
-    for _, value in ipairs(result.ancestorTitles) do
-      keyid = keyid .. "::" .. '"' .. value .. '"'
-    end
-    keyid = keyid .. "::" .. '"' .. name .. '"'
-    tests[keyid] = {
-      status = status,
-      short = name .. ": " .. status,
-      output = output_file,
-      location = result.location,
-    }
-    if result.failureMessages then
-      local errors = {}
-      for i, failMessage in ipairs(result.failureMessages) do
-        local msg = cleanAnsi(failMessage)
-        errors[i] = {
-          line = findErrorLine(result.location.line - 1, msg),
-          message = msg,
-        }
-        tests[keyid].short = tests[keyid].short .. "\n" .. msg
-      end
-      tests[keyid].errors = errors
-    end
-  end
-
-  local results = {}
+  local results, failed = parsed_json_to_results(parsed, output_file)
   for _, value in tree:iter() do
-    local test_output = tests[value.id]
-    if test_output == nil then
-      if value.type ~= "file" or value.type ~= "namespace" then
-        vim.notify("unable to find test result: " .. value.id)
-        vim.pretty_print(value)
-        return results
-      end
+    if value.type ~= 'file' or value.type ~= 'namespace' then
+      logger.error('Failed to find test result ', value)
+      return results
     end
-    results[value.id] = test_output
+    results[value.id] = {
+      status = 'passed',
+      output = output_file,
+    }
+    if failed then
+      results[value.id].status = 'failed'
+    end
   end
-  vim.pretty_print(results)
   return results
 end
 
