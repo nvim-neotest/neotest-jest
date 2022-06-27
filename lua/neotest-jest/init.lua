33b8b98ba2f481ed80387283fc8cfd1914f13ecf
adrigzr <a.gonzalezrus@gmail.com>
2022-06-27 13:50:49 +0200

    feat: allow multiple file tests

--- lua/neotest-jest/init.lua
+++ lua/neotest-jest/init.lua
@@ -1,6 +1,7 @@
 ---@diagnostic disable: undefined-field
 local lib = require("neotest.lib")
 local logger = require("neotest.logging")
+local util = require("lspconfig").util
 
 ---@type neotest.Adapter
 local adapter = { name = "neotest-jest" }
@@ -11,9 +12,11 @@ function adapter.is_test_file(file_path)
   if file_path == nil then
     return false
   end
+
   if string.match(file_path, "__tests__") then
     return true
   end
+
   for _, x in ipairs({ "spec", "test" }) do
     for _, ext in ipairs({ "js", "jsx", "coffee", "ts", "tsx" }) do
       if string.match(file_path, x .. "%." .. ext .. "$") then
@@ -21,6 +24,7 @@ function adapter.is_test_file(file_path)
       end
     end
   end
+
   return false
 end
 
@@ -28,29 +32,84 @@ end
 ---@return neotest.Tree | nil
 function adapter.discover_positions(path)
   local query = [[
-  ((call_expression
+    ; -- Namespaces --
+    ; Matches: `describe('context')`
+    ((call_expression
       function: (identifier) @func_name (#eq? @func_name "describe")
       arguments: (arguments (string (string_fragment) @namespace.name) (arrow_function))
-  )) @namespace.definition
+    )) @namespace.definition
+    ; Matches: `describe.only('context')`
+    ((call_expression
+      function: (member_expression
+        object: (identifier) @func_name (#any-of? @func_name "describe")
+      )
+      arguments: (arguments (string (string_fragment) @namespace.name) (arrow_function))
+    )) @namespace.definition
+    ; Matches: `describe.each(['data'])('context')`
+    ((call_expression
+      function: (call_expression
+        function: (member_expression
+          object: (identifier) @func_name (#any-of? @func_name "describe")
+        )
+      )
+      arguments: (arguments (string (string_fragment) @namespace.name) (arrow_function))
+    )) @namespace.definition
 
-
-  ((call_expression
+    ; -- Tests --
+    ; Matches: `test('test') / it('test')`
+    ((call_expression
       function: (identifier) @func_name (#any-of? @func_name "it" "test")
       arguments: (arguments (string (string_fragment) @test.name) (arrow_function))
-  ) ) @test.definition
-  ((call_expression
-      function: (member_expression) @func_name (#any-of? @func_name "it" "test")
+    )) @test.definition
+    ; Matches: `test.only('test') / it.only('test')`
+    ((call_expression
+      function: (member_expression
+        object: (identifier) @func_name (#any-of? @func_name "test" "it")
+      )
       arguments: (arguments (string (string_fragment) @test.name) (arrow_function))
-  ) ) @test.definition
-    ]]
+    )) @test.definition
+    ; Matches: `test.each(['data'])('test') / it.each(['data'])('test')`
+    ((call_expression
+      function: (call_expression
+        function: (member_expression
+          object: (identifier) @func_name (#any-of? @func_name "it" "test")
+        )
+      )
+      arguments: (arguments (string (string_fragment) @test.name) (arrow_function))
+    )) @test.definition
+  ]]
+
   return lib.treesitter.parse_positions(path, query, { nested_tests = true })
 end
 
-local function getJestCommand()
-  if vim.fn.filereadable("node_modules/.bin/jest") then
-    return "node_modules/.bin/jest"
+local function getJestCommand(path)
+  local rootPath = util.find_node_modules_ancestor(path)
+  local jestBinary = util.path.join(rootPath, "node_modules", ".bin", "jest")
+
+  if util.path.exists(jestBinary) then
+    return jestBinary
   end
+
   return "jest"
+end
+
+local jestConfigPattern = util.root_pattern("jest.config.{js,ts}")
+
+local function getJestConfig(path)
+  local rootPath = jestConfigPattern(path)
+
+  if not rootPath then
+    return nil
+  end
+
+  local jestJs = util.path.join(rootPath, "jest.config.js")
+  local jestTs = util.path.join(rootPath, "jest.config.ts")
+
+  if util.path.exists(jestTs) then
+    return jestTs
+  end
+
+  return jestJs
 end
 
 ---@param args neotest.RunArgs
@@ -58,39 +117,40 @@ end
 function adapter.build_spec(args)
   local results_path = vim.fn.tempname() .. ".json"
   local tree = args.tree
+
   if not tree then
     return
   end
+
   local pos = args.tree:data()
-  -- if pos.type == "dir" then
-  --   return
-  --roots
-  -- A list of paths to directories that Jest should use to search for files in.
-  -- end
-  local testNamePattern = ".*"
+  local testNamePattern = "'.*'"
+
   if pos.type == "test" then
-    testNamePattern = pos.name
+    testNamePattern = "'" .. pos.name:gsub("'", "") .. "$'"
   end
 
-  local binary = getJestCommand() or "jest"
-
+  local binary = getJestCommand(pos.path) or "jest"
+  local config = getJestConfig(pos.path) or "jest.config.js"
   local command = {}
+
   -- split by whitespace
   for w in binary:gmatch("%S+") do
     table.insert(command, w)
   end
+
   for _, value in ipairs({
     "--no-coverage",
     "--testLocationInResults",
     "--verbose",
     "--json",
     "--outputFile=" .. results_path,
+    "--config=" .. config,
     "--testNamePattern=" .. testNamePattern,
-    "--runTestsByPath",
     pos.path,
   }) do
     table.insert(command, value)
   end
+
   return {
     command = command,
     context = {
@@ -109,88 +169,92 @@ local function cleanAnsi(s)
     :gsub("\x1b%[%d+m", "")
 end
 
-local function findErrorLine(line, errStr)
-  local _, _, errLine = string.find(errStr, "(%d+)%:%d+")
-  if errLine then
-    return errLine - 1
-  end
-  return line
+local function findErrorPosition(file, errStr)
+  -- Look for: /path/to/file.js:123:987
+  local regexp = file:gsub("([^%w])", "%%%1") .. "%:(%d+)%:(%d+)"
+  local _, _, errLine, errColumn = string.find(errStr, regexp)
+
+  return errLine, errColumn
 end
 
 local function parsed_json_to_results(data, output_file)
   local tests = {}
-  local failed = false
 
-  local testFn = data.testResults[1].name
-  for _, result in pairs(data.testResults[1].assertionResults) do
-    local status, name = result.status, result.title
-    if name == nil then
-      logger.error("Failed to find parsed test result ", result)
-      return {}, failed
-    end
-    local keyid = testFn
-    for _, value in ipairs(result.ancestorTitles) do
-      keyid = keyid .. "::" .. value
-    end
-    keyid = keyid .. "::" .. name
-    if status == "pending" then
-      status = "skipped"
-    end
-    tests[keyid] = {
-      status = status,
-      short = name .. ": " .. status,
-      output = output_file,
-      location = result.location,
-    }
-    if result.failureMessages then
-      failed = true
-      local errors = {}
-      for i, failMessage in ipairs(result.failureMessages) do
-        local msg = cleanAnsi(failMessage)
-        errors[i] = {
-          line = findErrorLine(result.location.line - 1, msg),
-          message = msg,
-        }
-        tests[keyid].short = tests[keyid].short .. "\n" .. msg
+  for _, testResult in pairs(data.testResults) do
+    local testFn = testResult.name
+
+    for _, assertionResult in pairs(testResult.assertionResults) do
+      local status, name = assertionResult.status, assertionResult.title
+
+      if name == nil then
+        logger.error("Failed to find parsed test result ", assertionResult)
+        return {}
       end
-      tests[keyid].errors = errors
+
+      local keyid = testFn
+
+      for _, value in ipairs(assertionResult.ancestorTitles) do
+        keyid = keyid .. "::" .. value
+      end
+
+      keyid = keyid .. "::" .. name
+
+      if status == "pending" then
+        status = "skipped"
+      end
+
+      tests[keyid] = {
+        status = status,
+        short = name .. ": " .. status,
+        output = output_file,
+        location = assertionResult.location,
+      }
+
+      if not vim.tbl_isempty(assertionResult.failureMessages) then
+        local errors = {}
+
+        for i, failMessage in ipairs(assertionResult.failureMessages) do
+          local msg = cleanAnsi(failMessage)
+          local errorLine, errorColumn = findErrorPosition(testFn, msg)
+
+          errors[i] = {
+            line = (errorLine or assertionResult.location.line) - 1,
+            column = (errorColumn or 1) - 1,
+            message = msg,
+          }
+
+          tests[keyid].short = tests[keyid].short .. "\n" .. msg
+        end
+
+        tests[keyid].errors = errors
+      end
     end
   end
-  return tests, failed
+
+  return tests
 end
 
 ---@async
 ---@param spec neotest.RunSpec
----@param result neotest.StrategyResult
----@param tree neotest.Tree
 ---@return neotest.Result[]
 function adapter.results(spec, _, tree)
   local output_file = spec.context.results_path
   local success, data = pcall(lib.files.read, output_file)
+
   if not success then
     logger.error("No test output file found ", output_file)
     return {}
   end
+
   local ok, parsed = pcall(vim.json.decode, data, { luanil = { object = true } })
+
   if not ok then
     logger.error("Failed to parse test output json ", output_file)
     return {}
   end
 
-  local results, failed = parsed_json_to_results(parsed, output_file)
-  for _, value in tree:iter() do
-    if value.type ~= "file" or value.type ~= "namespace" then
-      logger.error("Failed to find test result ", value)
-      return results
-    end
-    results[value.id] = {
-      status = "passed",
-      output = output_file,
-    }
-    if failed then
-      results[value.id].status = "failed"
-    end
-  end
+  local results = parsed_json_to_results(parsed, output_file)
+
   return results
 end
 
