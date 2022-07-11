f80e85c3bac106d7f2e3e4e83e2bdaef9ef3a694
Hayden Meade <haydenmeade@gmail.com>
2022-07-11 10:47:34 +1000

    format

--- lua/neotest-jest/init.lua
+++ lua/neotest-jest/init.lua
@@ -114,19 +114,19 @@ end
 
 local function escapeTestPattern(s)
   return (
-      s
-          :gsub("%(", "%\\(")
-          :gsub("%)", "%\\)")
-          :gsub("%]", "%\\]")
-          :gsub("%[", "%\\[")
-          :gsub("%*", "%\\*")
-          :gsub("%+", "%\\+")
-          :gsub("%-", "%\\-")
-          :gsub("%?", "%\\?")
-          :gsub("%$", "%\\$")
-          :gsub("%^", "%\\^")
-          :gsub("%/", "%\\/")
-      )
+    s
+      :gsub("%(", "%\\(")
+      :gsub("%)", "%\\)")
+      :gsub("%]", "%\\]")
+      :gsub("%[", "%\\[")
+      :gsub("%*", "%\\*")
+      :gsub("%+", "%\\+")
+      :gsub("%-", "%\\-")
+      :gsub("%?", "%\\?")
+      :gsub("%$", "%\\$")
+      :gsub("%^", "%\\^")
+      :gsub("%/", "%\\/")
+  )
 end
 
 ---@param args neotest.RunArgs
@@ -179,11 +179,11 @@ end
 
 local function cleanAnsi(s)
   return s
-      :gsub("\x1b%[%d+;%d+;%d+;%d+;%d+m", "")
-      :gsub("\x1b%[%d+;%d+;%d+;%d+m", "")
-      :gsub("\x1b%[%d+;%d+;%d+m", "")
-      :gsub("\x1b%[%d+;%d+m", "")
-      :gsub("\x1b%[%d+m", "")
+    :gsub("\x1b%[%d+;%d+;%d+;%d+;%d+m", "")
+    :gsub("\x1b%[%d+;%d+;%d+;%d+m", "")
+    :gsub("\x1b%[%d+;%d+;%d+m", "")
+    :gsub("\x1b%[%d+;%d+m", "")
+    :gsub("\x1b%[%d+m", "")
 end
 
 local function findErrorPosition(file, errStr)
