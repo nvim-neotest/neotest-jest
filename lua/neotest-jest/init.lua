c3a06814143e506d44f65c74eaa7d1fb499c13c6
Jens Claes <jens@datacamp.com>
2022-10-06 09:12:33 +0200

    allow changing the strategy config when setting up adapter

--- lua/neotest-jest/init.lua
+++ lua/neotest-jest/init.lua
@@ -9,6 +9,7 @@ local util = require("neotest-jest.util")
 ---@field jestConfigFile? string|fun(): string
 ---@field env? table<string, string>|fun(): table<string, string>
 ---@field cwd? string|fun(): string
+---@field strategy_config? table<string, unknown>|fun(): table<string, unknown>
 
 ---@type neotest.Adapter
 local adapter = { name = "neotest-jest" }
@@ -146,7 +147,7 @@ local function escapeTestPattern(s)
   )
 end
 
-local function get_strategy_config(strategy, command, cwd)
+local function get_default_strategy_config(strategy, command, cwd)
   local config = {
     dap = function()
       return {
@@ -175,6 +176,10 @@ end
 ---@return string|nil
 local function getCwd(path)
   return nil
+end
+
+local function getStrategyConfig(default_strategy_config, args)
+  return default_strategy_config
 end
 
 ---@param args neotest.RunArgs
@@ -224,7 +229,10 @@ function adapter.build_spec(args)
       results_path = results_path,
       file = pos.path,
     },
-    strategy = get_strategy_config(args.strategy, command),
+    strategy = getStrategyConfig(
+      get_default_strategy_config(args.strategy, command, cwd) or {},
+      args
+    ),
     env = getEnv(args[2] and args[2].env or {}),
   }
 end
@@ -360,6 +368,13 @@ setmetatable(adapter, {
     elseif opts.cwd then
       getCwd = function()
         return opts.cwd
+      end
+    end
+    if is_callable(opts.strategy_config) then
+      getStrategyConfig = opts.strategy_config
+    elseif opts.strategy_config then
+      getStrategyConfig = function()
+        return opts.strategy_config
       end
     end
     return adapter
