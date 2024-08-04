5657759805ea6b08c716bfb7351305264c092c2a
Alexandr <mechanosec29@gmail.com>
2024-08-04 15:57:23 +0300

    Update init.lua

--- lua/neotest-jest/init.lua
+++ lua/neotest-jest/init.lua
@@ -416,6 +416,7 @@ function adapter.build_spec(args)
     "--json",
     "--outputFile=" .. results_path,
     "--testNamePattern=" .. testNamePattern,
+      "--forceExit",
     escapeTestPattern(vim.fs.normalize(pos.path)),
   })
 
