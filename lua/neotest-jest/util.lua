fb35084c33b94677c9f9159b908aba51aba44326
MisanthropicBit <proximity.dev@proton.me>
2025-06-15 21:17:01 +0200

    Fix infinite recursion when finding jest command

--- lua/neotest-jest/util.lua
+++ lua/neotest-jest/util.lua
@@ -188,7 +188,9 @@ function M.find_git_ancestor(startpath)
   return M.search_ancestors(startpath, function(path)
     -- .git is a file when the project is a git worktree
     -- or it's a directory if it's a regular project
-    if M.path.is_file(M.path.join(path, ".git")) or M.path.is_dir(M.path.join(path, ".git")) then
+    local git_path = M.path.join(path, ".git")
+
+    if M.path.is_file(git_path) or M.path.is_dir(git_path) then
       return path
     end
   end)
