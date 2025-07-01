5fd288af1c769f92bdb6675a1967e6c818a2e1ec
MisanthropicBit <proximity.dev@proton.me>
2025-07-01 22:27:23 +0200

    Try removing all test files but one

--- tests/adapter_root_spec.lua
+++
@@ -1,9 +0,0 @@
-local adapter = require("neotest-jest")({ jestCommand = "jest" })
-local async = require("nio").tests
-
-describe("adapter.root", function()
-  async.it("recognises root", function()
-    assert.Not.Nil(adapter.root("./spec"))
-    assert.Nil(adapter.root("."))
-  end)
-end)
