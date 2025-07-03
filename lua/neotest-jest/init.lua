7f2dddcf71e354b4dcdb6b21580850c09e0067cc
MisanthropicBit <proximity.dev@proton.me>
2025-07-03 21:38:39 +0200

    Try to await scheduler

--- lua/neotest-jest/init.lua
+++ lua/neotest-jest/init.lua
@@ -231,6 +231,8 @@ function adapter.discover_positions(path)
     )) @test.definition
   ]]
 
+  async.scheduler()
+
   local positions = lib.treesitter.parse_positions(path, query, {
     nested_tests = false,
     build_position = 'require("neotest-jest").build_position',
