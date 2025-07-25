96e69ffd41cf3d8aee312af6129347cdcb01882e
MisanthropicBit <proximity.dev@proton.me>
2025-07-25 15:32:25 +0200

    Fix comparison

--- lua/neotest-jest/compat.lua
+++ lua/neotest-jest/compat.lua
@@ -3,7 +3,7 @@ local compat = {}
 ---@param tbl table
 ---@return table
 function compat.tbl_flatten(tbl)
-  if vim.fn.has("nvim-0.13.0") then
+  if vim.fn.has("nvim-0.13.0") == 1 then
     return vim.iter(tbl):flatten():totable()
   end
 
