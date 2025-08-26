a1e31cf310331be8206a090b61edcb6c09bf9dba
MisanthropicBit <proximity.dev@proton.me>
2025-08-26 22:03:28 +0200

    Neotest is not needed

--- tests/minimal_init.lua
+++ tests/minimal_init.lua
@@ -1,7 +1,6 @@
 -- Update the paths below to match your local installations of the plugins
 vim.opt.rtp:append(".")
 vim.opt.rtp:append("~/.local/share/nvim/lazy/plenary.nvim")
-vim.opt.rtp:append("~/.local/share/nvim/lazy/neotest")
 vim.opt.rtp:append("~/.local/share/nvim/lazy/nvim-nio")
 vim.opt.rtp:append("~/.local/share/nvim/lazy/nvim-treesitter")
 
