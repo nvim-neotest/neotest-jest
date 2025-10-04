289830b82d50436d6a6dc27e480bc385550d9809
MisanthropicBit <proximity.dev@proton.me>
2025-10-04 22:33:42 +0200

    Improve discover_positions tests, add test

--- tests/adapter_discover_positions_spec.lua
+++ tests/adapter_discover_positions_spec.lua
@@ -1,208 +1,333 @@
 local adapter = require("neotest-jest")({ jestCommand = "jest" })
 local async = require("nio").tests
-local stub = require("luassert.stub")
 local test_utils = require("neotest-jest.test-utils")
+local neotest_types = require("neotest.types")
+
+local PositionType = neotest_types.PositionType
 
 test_utils.prepare_vim_treesitter()
 
 describe("adapter.discover_positions", function()
-  local assert_test_positions_match = function(expected_output, positions)
-    for i, value in ipairs(expected_output) do
-      assert.is.truthy(value)
-      local position = positions[i + 1][1]
-      assert.is.truthy(position)
-      assert.equals(value.name, position.name)
-      assert.equals(value.type, position.type)
-    end
-  end
+  assert:set_parameter("TableFormatLevel", 10)
 
   async.it("provides meaningful names from a basic spec", function()
-    local positions = adapter.discover_positions("./spec/tests/basic.test.ts"):to_list()
+    local path = "./spec/tests/basic.test.ts"
+    local positions = adapter.discover_positions(path):to_list()
 
     local expected_output = {
       {
+        id = path,
         name = "basic.test.ts",
-        type = "file",
+        path = path,
+        range = { 0, 0, 47, 0 },
+        type = PositionType.file,
       },
       {
         {
+          id = path .. "::describe text",
+          is_parameterized = false,
           name = "describe text",
-          type = "namespace",
+          path = path,
+          range = { 4, 0, 24, 2 },
+          type = PositionType.namespace,
         },
         {
-          name = "1",
-          type = "test",
+          {
+            id = path .. "::describe text::1",
+            is_parameterized = false,
+            name = "1",
+            path = path,
+            range = { 5, 2, 7, 4 },
+            type = PositionType.test,
+          },
         },
         {
-          name = "2",
-          type = "test",
+          {
+            id = path .. "::describe text::2",
+            is_parameterized = false,
+            name = "2",
+            path = path,
+            range = { 9, 2, 11, 4 },
+            type = PositionType.test,
+          },
         },
         {
-          name = "3",
-          type = "test",
+          {
+            id = path .. "::describe text::3",
+            is_parameterized = false,
+            name = "3",
+            path = path,
+            range = { 13, 2, 15, 4 },
+            type = PositionType.test,
+          },
         },
         {
-          name = "4",
-          type = "test",
+          {
+            id = path .. "::describe text::4",
+            is_parameterized = false,
+            name = "4",
+            path = path,
+            range = { 17, 2, 19, 4 },
+            type = PositionType.test,
+          },
         },
         {
-          name = "5",
-          type = "test",
+          {
+            id = path .. "::describe text::5",
+            is_parameterized = false,
+            name = "5",
+            path = path,
+            range = { 21, 2, 23, 5 },
+            type = PositionType.test,
+          },
         },
       },
       {
         {
+          id = path .. "::describe text 2",
+          is_parameterized = false,
           name = "describe text 2",
-          type = "namespace",
+          path = path,
+          range = { 26, 0, 46, 2 },
+          type = PositionType.namespace,
         },
         {
-          name = "1",
-          type = "test",
+          {
+            id = path .. "::describe text 2::1",
+            is_parameterized = false,
+            name = "1",
+            path = path,
+            range = { 27, 2, 29, 4 },
+            type = PositionType.test,
+          },
         },
         {
-          name = "2",
-          type = "test",
+          {
+            id = path .. "::describe text 2::2",
+            is_parameterized = false,
+            name = "2",
+            path = path,
+            range = { 31, 2, 33, 4 },
+            type = PositionType.test,
+          },
         },
         {
-          name = "3",
-          type = "test",
+          {
+            id = path .. "::describe text 2::3",
+            is_parameterized = false,
+            name = "3",
+            path = path,
+            range = { 35, 2, 37, 4 },
+            type = PositionType.test,
+          },
         },
         {
-          name = "4",
-          type = "test",
+          {
+            id = path .. "::describe text 2::4",
+            is_parameterized = false,
+            name = "4",
+            path = path,
+            range = { 39, 2, 41, 4 },
+            type = PositionType.test,
+          },
         },
         {
-          name = "5",
-          type = "test",
+          {
+            id = path .. "::describe text 2::5",
+            is_parameterized = false,
+            name = "5",
+            path = path,
+            range = { 43, 2, 45, 5 },
+            type = PositionType.test,
+          },
         },
       },
     }
 
-    assert.equals(expected_output[1].name, positions[1].name)
-    assert.equals(expected_output[1].type, positions[1].type)
-    assert.equals(expected_output[2][1].name, positions[2][1].name)
-    assert.equals(expected_output[2][1].type, positions[2][1].type)
-
-    assert.equals(positions[2][1].is_parameterized, false)
-    assert.equals(6, #positions[2])
-
-    for i, value in ipairs(expected_output[2][2]) do
-      assert.is.truthy(value)
-      local position = positions[2][i + 1][1]
-      assert.is.truthy(position)
-      assert.equals(value.name, position.name)
-      assert.equals(value.type, position.type)
-      assert.equals(position.is_parameterized, false)
-    end
-
-    assert.equals(expected_output[3][1].name, positions[3][1].name)
-    assert.equals(expected_output[3][1].type, positions[3][1].type)
-
-    assert.equals(6, #positions[2])
-    assert_test_positions_match(expected_output[2][2], positions[2])
-
-    assert.equals(6, #positions[3])
-    assert_test_positions_match(expected_output[3][2], positions[3])
+    assert.are.same(positions, expected_output)
   end)
 
   async.it("provides meaningful names for parametric tests", function()
-    stub(require("neotest.lib").process, "run")
-    local positions = adapter.discover_positions("./spec/tests/array.test.ts"):to_list()
+    local path = "./spec/tests/array.test.ts"
+    local positions = adapter.discover_positions(path):to_list()
 
     local expected_output = {
       {
+        id = path,
         name = "array.test.ts",
-        type = "file",
-        is_parameterized = false,
+        path = path,
+        range = { 0, 0, 35, 0 },
+        type = PositionType.file,
       },
       {
         {
-          name = "describe text",
-          type = "namespace",
+          id = path .. "::describe text",
           is_parameterized = false,
+          name = "describe text",
+          path = path,
+          range = { 0, 0, 16, 2 },
+          type = PositionType.namespace,
         },
         {
           {
+            id = path .. "::describe text::Array1 %d",
+            is_parameterized = true,
             name = "Array1 %d",
-            type = "test",
-            is_parameterized = true,
+            path = path,
+            range = { 1, 2, 3, 4 },
+            type = PositionType.test,
           },
         },
         {
           {
+            id = path .. "::describe text::Array2",
+            is_parameterized = true,
             name = "Array2",
-            type = "test",
-            is_parameterized = true,
+            path = path,
+            range = { 5, 2, 7, 4 },
+            type = PositionType.test,
           },
         },
         {
           {
+            id = path .. "::describe text::Array3 %d",
+            is_parameterized = true,
             name = "Array3 %d",
-            type = "test",
-            is_parameterized = true,
+            path = path,
+            range = { 9, 2, 11, 4 },
+            type = PositionType.test,
           },
         },
         {
           {
+            id = path .. "::describe text::Array4 %d",
+            is_parameterized = true,
             name = "Array4 %d",
-            type = "test",
+            path = path,
+            range = { 13, 2, 15, 4 },
+            type = PositionType.test,
           },
         },
       },
       {
         {
+          id = path .. "::describe text 2",
+          is_parameterized = false,
           name = "describe text 2",
-          type = "namespace",
+          path = path,
+          range = { 18, 0, 34, 2 },
+          type = PositionType.namespace,
         },
         {
           {
-            name = "Array1 %d",
-            type = "test",
-          },
-        },
-        {
-          {
-            name = "Array2 %d",
-            type = "test",
-          },
-        },
-        {
-          {
-            name = "Array3 %d",
-            type = "test",
-          },
-        },
-        {
-          {
-            name = "Array4",
-            type = "test",
+            id = path .. "::describe text 2::Array1 %d",
             is_parameterized = true,
+            name = "Array1 %d",
+            path = path,
+            range = { 19, 2, 21, 4 },
+            type = PositionType.test,
+          },
+        },
+        {
+          {
+            id = path .. "::describe text 2::Array2 %d",
+            is_parameterized = true,
+            name = "Array2 %d",
+            path = path,
+            range = { 23, 2, 25, 4 },
+            type = PositionType.test,
+          },
+        },
+        {
+          {
+            id = path .. "::describe text 2::Array3 %d",
+            is_parameterized = true,
+            name = "Array3 %d",
+            path = path,
+            range = { 27, 2, 29, 4 },
+            type = PositionType.test,
+          },
+        },
+        {
+          {
+            id = path .. "::describe text 2::Array4",
+            is_parameterized = true,
+            name = "Array4",
+            path = path,
+            range = { 31, 2, 33, 4 },
+            type = PositionType.test,
           },
         },
       },
     }
-    assert.equals(expected_output[1].name, positions[1].name)
-    assert.equals(expected_output[1].type, positions[1].type)
-    assert.equals(expected_output[2][1].name, positions[2][1].name)
-    assert.equals(expected_output[2][1].type, positions[2][1].type)
 
-    assert.equals(expected_output[2][1].is_parameterized, positions[2][1].is_parameterized)
-
-    for i, value in ipairs(expected_output[2][2]) do
-      assert.is.truthy(value)
-      local position = positions[2][i + 1][1]
-      assert.is.truthy(position)
-      assert.equals(value.name, position.name)
-      assert.equals(value.type, position.type)
-      assert.equals(value.is_parameterized, position.is_parameterized)
-    end
-
-    assert.equals(5, #positions[2])
-    assert_test_positions_match(expected_output[2][2], positions[2])
-
-    assert.equals(5, #positions[3])
-    assert_test_positions_match(expected_output[3][2], positions[3])
+    assert.are.same(positions, expected_output)
   end)
 
-  async.it("provides meaningful names for parametric describe", function() end)
+  async.it("provides meaningful names for parametric describe", function()
+    local path = "./spec/tests/parametric-describes-only.test.ts"
+    local positions = adapter.discover_positions(path):to_list()
+
+    local expected_output = {
+      {
+        id = path,
+        name = "parametric-describes-only.test.ts",
+        path = path,
+        range = { 0, 0, 15, 0 },
+        type = PositionType.file,
+      },
+      {
+        {
+          id = path .. "::is it enabled? [%s]",
+          is_parameterized = true,
+          name = "is it enabled? [%s]",
+          path = path,
+          range = { 0, 0, 14, 2 },
+          type = PositionType.namespace,
+        },
+        {
+          {
+            id = path .. "::is it enabled? [%s]::how many?: %d",
+            is_parameterized = true,
+            name = "how many?: %d",
+            path = path,
+            range = { 1, 2, 13, 4 },
+            type = PositionType.namespace,
+          },
+          {
+            {
+              id = path .. "::is it enabled? [%s]::how many?: %d::test 1",
+              is_parameterized = false,
+              name = "test 1",
+              path = path,
+              range = { 2, 4, 4, 6 },
+              type = PositionType.test,
+            },
+          },
+          {
+            {
+              id = path .. "::is it enabled? [%s]::how many?: %d::test 2",
+              is_parameterized = false,
+              name = "test 2",
+              path = path,
+              range = { 6, 4, 8, 6 },
+              type = PositionType.test,
+            },
+          },
+          {
+            {
+              id = path .. "::is it enabled? [%s]::how many?: %d::test 3",
+              is_parameterized = false,
+              name = "test 3",
+              path = path,
+              range = { 10, 4, 12, 6 },
+              type = PositionType.test,
+            },
+          },
+        },
+      },
+    }
+
+    assert.are.same(positions, expected_output)
+  end)
 end)
