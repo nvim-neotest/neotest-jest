d4c2e459281cf29c14812be24dc5561fcf165740
MisanthropicBit <proximity.dev@proton.me>
2025-10-15 14:44:38 +0200

    See if it is a typescript issue

--- spec/tests/parametricDescribeAndTest.test.ts
+++ spec/tests/parametricDescribeAndTest.test.ts
@@ -1,12 +1,8 @@
-function greet(name: string, salutation: string): string {
-  return `${salutation}, ${name}!`
-}
-
 describe.each([['Alice'], ['Bob']])('greeting %s', (name) => {
   it.each([
     ['Hello'],
     ['Hi'],
   ])('should greet using %s!', (salutation) => {
-    expect(greet(name, salutation)).toBe(`${salutation}, ${name}!`);
+    expect(`${salutation}, ${name}!`).toBe(`${salutation}, ${name}!`);
   });
 });
