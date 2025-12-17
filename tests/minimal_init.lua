2a7d3f8c9fea62b852c1e9c38d260aa9796361f3
MisanthropicBit <proximity.dev@proton.me>
2025-12-17 13:49:26 +0100

    Print install parsers

--- tests/minimal_init.lua
+++ tests/minimal_init.lua
@@ -9,3 +9,5 @@ vim.cmd.runtime({ "plugin/plenary.vim", bang = true })
 require("neotest").setup({
   log_level = vim.log.levels.WARN,
 })
+
+vim.print(vim.inspect(require("nvim-treesitter.config").get_installed("parsers")))
